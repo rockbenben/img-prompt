49c7074c4b0eb813d409dfcb7eda959a601e3494
rockbenben <qingwhat@gmail.com>
2023-04-18 09:56:54 +0800

    chore: update tailwindcss 3 configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  purge: ["./src/**/*.jsx", "./src/**/*.js"],
-  darkMode: false,
+  content: ["./src/**/*.jsx", "./src/**/*.js"],
   theme: {
     extend: {},
   },
