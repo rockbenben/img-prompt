754c28e099dc88696aebe62ab7c6c81d9ed1d911
rockbenben <qingwhat@gmail.com>
2025-12-09 17:09:07 +0800

    Delete DataContext.tsx

--- src/app/utils/DataContext.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React, { createContext, ReactNode } from "react";
-import { TagItem } from "@/app/components/types";
-
-export const DataContext = createContext<TagItem[]>([]);
-
-export function DataProvider({ children, data }: { children: ReactNode; data: TagItem[] }) {
-  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
-}
